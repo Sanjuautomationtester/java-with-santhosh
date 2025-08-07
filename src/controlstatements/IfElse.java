7b0f40014f28fdaaa81e4e4fecde6b589bc6550d
santhosh <santhpm@gmail.com>
2025-08-07 17:26:06 -0400

    If-else condition

---
+++ src/controlstatements/IfElse.java
@@ -0,0 +1,29 @@
+package controlstatements;
+
+public class IfElse {
+
+	public static void main(String[] args) {
+		int number = 20;
+		
+		if(number>10) {
+			System.out.println("condition passed");
+		}
+			else if(number<30) {
+				System.out.println("this is also passed");
+	}
+			else {
+			System.out.println("Condition failed");
+			}
+	
+	
+	int age = 18;
+	if(age<18) {
+	
+		System.out.println("a person can vote");
+	}
+		else {
+			System.out.println("a person cannot vote");
+		}
+	}
+}
+
